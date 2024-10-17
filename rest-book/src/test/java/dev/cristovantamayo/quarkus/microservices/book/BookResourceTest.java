ed00ffdb5d9ffdbfaad3f5bd4e25224f6a7f59f7
Cristovan Tamayo <ctamayo@bionexo.com>
2024-10-16 22:26:03 -0300

    BookResource Test created

--- rest-book/src/test/java/dev/cristovantamayo/quarkus/microservices/book/BookResourceTest.java
+++ rest-book/src/test/java/dev/cristovantamayo/quarkus/microservices/book/BookResourceTest.java
@@ -5,16 +5,27 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 class BookResourceTest {
     @Test
-    void testHelloEndpoint() {
+    void shouldCreateABookTest() {
         given()
-          .when().get("/api/books")
+                .formParam("title", "The Hobbit")
+                .formParam("author", "J.R.R. Tolkien")
+                .formParam("year", 1937)
+                .formParam("genre", "Fantasy")
+          .when().post("/api/books")
           .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .statusCode(201)
+             .body("isbn_13", startsWith("13-"))
+             .body("title", is("The Hobbit"))
+             .body("author", is("J.R.R. Tolkien"))
+             .body("year-of-publication", is(1937))
+             .body("genre", is("Fantasy"))
+             .body("creation-date", startsWith("20"));
+
     }
 
 }
