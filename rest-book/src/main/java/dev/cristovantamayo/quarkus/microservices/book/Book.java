6ddaac547a8d9aa4154ae06d1dc45557f5acc0b3
Cristovan Tamayo <ctamayo@bionexo.com>
2024-10-16 21:38:48 -0300

    Rest-Book Service Module

---
+++ rest-book/src/main/java/dev/cristovantamayo/quarkus/microservices/book/Book.java
@@ -0,0 +1,37 @@
+package dev.cristovantamayo.quarkus.microservices.book;
+
+import jakarta.json.bind.annotation.JsonbCreator;
+import jakarta.json.bind.annotation.JsonbDateFormat;
+import jakarta.json.bind.annotation.JsonbProperty;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import java.time.Instant;
+@Schema(description = "Book information")
+public class Book {
+
+    @JsonbProperty("isbn_13")
+    @Schema(required = true)
+    public String isbn13;
+    @Schema(required = true)
+    public String title;
+    public String author;
+    @JsonbProperty("year-of-publication")
+    public int yearOfPublication;
+    public String genre;
+    @JsonbDateFormat("YYYY-MM-DD")
+    @JsonbProperty("creation-date")
+    @Schema(implementation = String.class, format = "date")
+    public Instant creationDate;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn13='" + isbn13 + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearOfPublication=" + yearOfPublication +
+                ", genre='" + genre + '\'' +
+                ", creationDate=" + creationDate +
+                '}';
+    }
+}
--- rest-book/src/main/java/dev/cristovantamayo/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/dev/cristovantamayo/quarkus/microservices/book/BookResource.java
@@ -1,16 +1,41 @@
 package dev.cristovantamayo.quarkus.microservices.book;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
 
 @Path("/api/books")
+@Tag(name = "Book REST Endpoint", description = "Create Books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    @Inject
+    Logger logger;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Operation(
+            summary = "Create a new book",
+            description = "Create a new book with the given title, author, year of publication, and genre"
+    )
+    public Response createBook(
+            @FormParam("title") String title,
+            @FormParam("author") String author,
+            @FormParam("year") int yearOfPublication,
+            @FormParam("genre") String genre) {
+        Book book = new Book();
+        book.isbn13 = "13-123456789";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = java.time.Instant.now();
+
+        logger.info("Created book: "+book);
+        return Response.status(201).entity(book).build();
     }
 }
